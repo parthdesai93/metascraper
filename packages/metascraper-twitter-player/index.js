1b1820e52429bf1fdbb49f70f4924d8d4ef74eb1
Parth Desai <psdesai.93@gmail.com>
2018-01-10 14:21:55 -0500

    changes

--- packages/metascraper-twitter-player/index.js
+++ packages/metascraper-twitter-player/index.js
@@ -1,20 +1,30 @@
-'use strict';
+'use strict'
 
-const { getUrl, isUrl } = require('@metascrapper/helpers');
-const { isString } = require('lodash');
+const { getUrl, isUrl } = require('@metascraper/helpers')
+const { isString } = require('lodash')
 
+/**
+ * Wrap a rule with validation and formatting logic.
+ *
+ * @param {Function} rule
+ * @return {Function} wrapped
+ */
 
 const wrap = rule => ({ htmlDom, url }) => {
-    const value = rule(htmlDom, url);
-    return isUrl(value) ? getUrl(value, url) : url;
+  const value = rule(htmlDom, url)
+  return isUrl(value) ? getUrl(value, url) : url
 }
 
+/**
+ * Rules.
+ */
 
 module.exports = () => ({
-    playerUrl: [
-        wrap($ => $('meta[name="twitter:player"]').attr('value')),
-        ($, url) => (isString(url) ? url : null)
-    ]
-});
-
-
+  twitterPlayer: [
+    wrap($ => $('meta[property="og:url"]').attr('content')),
+    wrap($ => $('meta[name="twitter:url"]').attr('content')),
+    wrap($ => $('link[rel="canonical"]').attr('href')),
+    wrap($ => $('link[rel="alternate"][hreflang="x-default"]').attr('href')),
+    ($, url) => (isString(url) ? url : null)
+  ]
+})
